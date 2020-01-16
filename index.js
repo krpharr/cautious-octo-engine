d2e51ba9c902d870e9be2f154d793ba5689dc58e
rpharr <rpharr@richmond.edu>
2020-01-16 00:29:45 -0500

    added more styling

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ inquirer
                 }
                 //////////////////////////////////
 
+                let mapUrl = `https://www.google.com/maps/place/${location.replace(" ", "+")}`;
+
                 let htmlStr =
                     `<!DOCTYPE html>
 <html lang="en">
@@ -70,6 +72,7 @@ inquirer
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/css/style.css">
     <title>${name}</title>
 </head>
@@ -79,27 +82,34 @@ inquirer
             <img src="${image}">
             <div class="name-location bdr">
                 <h1  class="info">${name}</h1>
-                <div class="info">${location}</div>
+                <div class="info"><a href="${mapUrl}" targer="_blank"><i class="material-icons">near_me</i>${location}</a></div>
             </div>
         </div>
         <div class="bio info bdr">${bio}</div>
-        <div class="links">
-            <div class="info bdr"><a href=${profile}>${profile}</a></div>
-            <div class="info bdr"><a href=${blog}>${blog}</a></div>
+        <div class="links bdr">
+            <div class="info">
+                <div class="github-user">
+                    <img class="github-logo" src="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/images/github.png">
+                    <h5>GitHub: ${username}</h5>
+                </div>
+                <a class="link" href="${profile}">${profile}</a>
+            </div>
+            <div class="info">
+                <h5>Blog:</h5>
+                <a class="link" href="${blog}">${blog}</a>
+            </div>
         </div>
         <div class="stats">
-            <div class="info bdr">Repositories: ${numRepos}</div>
-            <div class="info bdr">Followers: ${followers}</div>
-            <div class="info bdr">Following: ${following}</div>
-            <div class="info bdr">Stars: todo</div>
+            <div class="info bdr stat">Repositories: ${numRepos}</div>
+            <div class="info bdr stat">Stars: 0</div>
+        </div>
+        <div class="stats">
+            <div class="info bdr stat">Followers: ${followers}</div>
+            <div class="info bdr stat">Following: ${following}</div>
         </div>
     </div>
 </body>
 </html>`;
-
-
-
-                // console.log(htmlStr);
 
                 var conversion = convertFactory({
                     converterPath: convertFactory.converters.PDF,
