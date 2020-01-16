b46220e8ac9ed9a4417d5eb282d7e7fcd5c1e514
rpharr <rpharr@richmond.edu>
2020-01-16 13:31:51 -0500

    solved stars problem addgif of working program

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ inquirer
         name: "favColor"
     }])
     .then(function({ username, favColor }) {
-        console.log(favColor);
+        // console.log(favColor);
         const queryUrl = `https://api.github.com/users/${username}`;
 
         axios
@@ -44,88 +44,121 @@ inquirer
                 ${followers}
                 ${following}
                 ${starred_url}`;
-                console.log(str);
+                // console.log(str);
                 //////////////////////////////
                 // TODO work out async on getting stars set 
-                let numPages = Math.ceil(numRepos / 100);
-                var stars = 0;
-                for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-                    const queryRepos = `https://api.github.com/users/${username}/repos?page=${pageNum}&per_page=100`;
-                    axios
-                        .get(queryRepos)
-                        .then(res => {
-                            // console.log(res.data);
-                            res.data.forEach(repo => {
-                                stars += parseInt(repo.stargazers_count);
-                                console.log(repo.name, repo.stargazers_count, stars);
-                            })
-                        });
-                }
-                //////////////////////////////////
 
-                let mapUrl = `https://www.google.com/maps/place/${location.replace(" ", "+")}`;
+                // var stars = 0;
+                // for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+                //     const queryRepos = `https://api.github.com/users/${username}/repos?page=${pageNum}&per_page=100`;
+                //     axios
+                //         .get(queryRepos)
+                //         .then(res => {
+                //             res.data.forEach(repo => {
+                //                 stars += parseInt(repo.stargazers_count);
+                //                 console.log(repo.name, repo.stargazers_count, stars);
+                //             })
+                //         });
+                // }
 
-                let htmlStr =
-                    `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/css/style.css">
-    <title>${name}</title>
-</head>
-<body style="background-color: ${favColor};">
-    <div class="container">
-        <div class="headerStyle">
-            <img src="${image}">
-            <div class="name-location bdr">
-                <h1  class="info">${name}</h1>
-                <div class="info"><a href="${mapUrl}" targer="_blank"><i class="material-icons">near_me</i>${location}</a></div>
-            </div>
-        </div>
-        <div class="bio info bdr">${bio}</div>
-        <div class="links bdr">
-            <div class="info">
-                <div class="github-user">
-                    <img class="github-logo" src="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/images/github.png">
-                    <h5>GitHub: ${username}</h5>
-                </div>
-                <a class="link" href="${profile}">${profile}</a>
-            </div>
-            <div class="info">
-                <h5>Blog:</h5>
-                <a class="link" href="${blog}">${blog}</a>
-            </div>
-        </div>
-        <div class="stats">
-            <div class="info bdr stat">Repositories: ${numRepos}</div>
-            <div class="info bdr stat">Stars: 0</div>
-        </div>
-        <div class="stats">
-            <div class="info bdr stat">Followers: ${followers}</div>
-            <div class="info bdr stat">Following: ${following}</div>
-        </div>
-    </div>
-</body>
-</html>`;
+                async function getNumStars(user, nPages) {
+                    let numStars = 0;
+                    try {
+                        for (let pageNum = 1; pageNum <= nPages; pageNum++) {
+                            // let repos = await getReposFromPage(user, pageNum);
+                            let { data } = await axios.get(`https://api.github.com/users/${user}/repos?page=${pageNum}&per_page=100`);
+                            // console.log("getNumStars: repos", data);
+                            numStars += loopRepos(data);
+                            // console.log("numStars", numStars);
+                        }
+                        return numStars;
 
-                var conversion = convertFactory({
-                    converterPath: convertFactory.converters.PDF,
-                    allowLocalFilesAccess: true
-                });
-
-                conversion({ html: htmlStr }, function(err, result) {
-                    if (err) {
-                        return console.error(err);
+                    } catch (err) {
+                        console.log(err);
                     }
 
-                    console.log(result.numberOfPages);
-                    console.log(result.logs);
-                    result.stream.pipe(fs.createWriteStream(`${githubName}.pdf`));
-                    conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+                };
+
+                function loopRepos(repos) {
+                    let n = 0;
+                    repos.forEach(r => {
+                        n += parseInt(r.stargazers_count);
+                    });
+                    return n;
+                };
+
+                let stars = 0;
+                let numPages = Math.ceil(numRepos / 100);
+                getNumStars(username, numPages).then(res => {
+                    stars = res;
+                    let mapUrl = `https://www.google.com/maps/place/${location.replace(" ", "+")}`;
+
+                    let htmlStr =
+                        `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/css/style.css">
+        <title>${name}</title>
+    </head>
+    <body style="background-color: ${favColor};">
+        <div class="container">
+            <div class="headerStyle">
+                <img src="${image}">
+                <div class="name-location bdr">
+                    <h1  class="info">${name}</h1>
+                    <div class="info"><a href="${mapUrl}" targer="_blank"><i class="material-icons">near_me</i>${location}</a></div>
+                </div>
+            </div>
+            <div class="bio info bdr">${bio}</div>
+            <div class="links bdr">
+                <div class="info">
+                    <div class="github-user">
+                        <img class="github-logo" src="/Users/randallpharr/bootcamp/cautious-octo-engine/assets/images/github.png">
+                        <h5>GitHub: ${username}</h5>
+                    </div>
+                    <a class="link" href="${profile}">${profile}</a>
+                </div>
+                <div class="info">
+                    <h5>Blog:</h5>
+                    <a class="link" href="${blog}">${blog}</a>
+                </div>
+            </div>
+            <div class="stats">
+                <div class="info bdr stat">Repositories: ${numRepos}</div>
+                <div class="info bdr stat">Stars: ${stars}</div>
+            </div>
+            <div class="stats">
+                <div class="info bdr stat">Followers: ${followers}</div>
+                <div class="info bdr stat">Following: ${following}</div>
+            </div>
+        </div>
+    </body>
+    </html>`;
+
+                    var conversion = convertFactory({
+                        converterPath: convertFactory.converters.PDF,
+                        allowLocalFilesAccess: true
+                    });
+
+                    conversion({ html: htmlStr }, function(err, result) {
+                        if (err) {
+                            return console.error(err);
+                        }
+
+                        // console.log(result.numberOfPages);
+                        // console.log(result.logs);
+                        result.stream.pipe(fs.createWriteStream(`${githubName}.pdf`));
+                        conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+                    });
                 });
+
+                //////////////////////////////////
+
+
 
             });
 
